e276c4a80e8093dc7ad5050f4e733535e1404035
Blundell89 <leeblundell16@gmail.com>
2016-01-28 16:09:30 +0000

    Fixed npm error

--- src/flapper-news/app.js
+++ src/flapper-news/app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+require('./models/Posts');
+require('./models/Comments');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -56,10 +60,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-var mongoose = require('mongoose');
 
-require('./models/Posts');
-require('./models/Comments');
 
 mongoose.connect('mongodb://localhost/news');
 
