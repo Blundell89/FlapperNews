7cb43ce8eb757f0b1fde35540ee0b0ef021ec58f
Blundell89 <leeblundell16@gmail.com>
2016-02-12 09:04:52 +0000

    Added endpoint

--- src/flapper-news/routes/index.js
+++ src/flapper-news/routes/index.js
@@ -32,4 +32,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
 module.exports = router;
